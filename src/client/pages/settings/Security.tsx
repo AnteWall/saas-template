70c8e59fe8dc84f1d60146b5ec67b794a3c1c8a3
Ante Wall <antewall@gmail.com>
2025-01-07 14:55:18 +0100

    Fix responsive design and security container styling

--- src/client/pages/settings/Security.tsx
+++ src/client/pages/settings/Security.tsx
@@ -1,15 +1,26 @@
-import { Title, Container } from "@mantine/core";
 import { SplitSection } from "../../components/common/SplitSection";
 import { SessionList } from "../../components/settings/SessionList";
 import React from "react";
 import { HelmetWrapper } from "@/components/common/HelmetWrapper";
+import { Container } from "@/components/ui/container";
+import { TypographyLarge } from "@/components/ui/typography";
+import { BreadcrumbsHeader } from "@/components/layout/breadcrumbs-header";
+import { paths } from "../paths";
 
 export const Security: React.FC = () => {
   return (
     <>
       <HelmetWrapper title="Security" />
-      <Container mt="60">
-        <Title>Security</Title>
+      <BreadcrumbsHeader
+        breadcrumbs={[
+          { label: "Settings", to: paths.Settings },
+          {
+            label: "Security",
+          },
+        ]}
+      />
+      <Container>
+        <TypographyLarge>Security</TypographyLarge>
         <SplitSection
           title="Active sessions"
           description="Manage your active sessions on other devices"
